75cc0400453678ee77a98bc63763c1a13ef1d51c
Jim Traugott <jtraug@gmail.com>
2016-01-21 16:00:01 -0800

    Added lint fixes

--- client/graph.js
+++ client/graph.js
@@ -7,31 +7,31 @@ var Graph = function ( config) {
     this.config.max = 0;
     this.config.min = 0;
     this.config.Xscale = 0;
-    this.config.Yscale = .001;
+    this.config.Yscale = 0.001;
     this.config.elements = 0;
     
-}
+};
 
 Graph.prototype.elements = function() {
     return this.config.elements;
-}
+};
 
 Graph.prototype.Xscale = function() {
     return this.config.Xscale;
-}
+};
 
 
 Graph.prototype.Yscale = function() {
     return this.config.Yscale;
-}
+};
 
 Graph.prototype.width = function() {
     return this.config.width;
-}
+};
 
 Graph.prototype.height = function() {
     return this.config.height;
-}
+};
 // control min/max settings for height:
 Graph.prototype.setMinMax =  function(numarr) {
     var config = this.config;
@@ -55,19 +55,19 @@ Graph.prototype.setMinMax =  function(numarr) {
             }
         }
     });
-    this.config = config
+    this.config = config;
     this.config.elements = elements;
     this.config.Xscale = Math.floor(this.config.width/this.config.elements);
     this.config.Yscale = Math.floor(this.config.height/(this.config.max - this.config.min));
-}
+};
 
 Graph.prototype.max = function() {
     return Math.ceil(this.config.max);
-}
+};
 
 Graph.prototype.min = function() {
     return  Math.floor(this.config.min);
-}
+};
 
 //module.exports.Graph = Graph;
 
