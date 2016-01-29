e78b7aa639341c62f4e4137fa6fbf1fe117daed7
Jim Traugott <jtraug@gmail.com>
2016-01-29 14:40:34 -0800

    Added time axis grid and legend

--- client/drawgraph.js
+++ client/drawgraph.js
@@ -4,20 +4,12 @@ var drawGraph = function() {
 
     context.beginPath();
         context.lineWidth = lineWidth;
-    var config = {height: 500, width: 1000};
+    var config = {height: 500, width: 800};
     var graph = new Graph(config);
     graph.setMinMax(rdata);
     //do legend:
-    context.strokeStyle = "#8a9194";
-    var elem = graph.min();
-    while (elem <= graph.max()) { 
-        var Selem = elem - 10;
-        context.moveTo(25, 500  - (Selem * graph.Yscale())); 
-        context.lineTo(1000, 500 - (Selem * graph.Yscale() )); 
-        context.fillText(elem, 10, 500 - (Selem * graph.Yscale()));
-        elem +=1; 
-    } 
-    context.stroke();
+    doLegend(graph, rdata);
+
     context.beginPath();
     var fudge = 10;
     context.strokeStyle = '#1D8DB3';
@@ -29,5 +21,37 @@ var drawGraph = function() {
         context.lineTo(100 + amount, 500 -( Math.ceil(graph.Yscale() * (element.height- fudge))));
         amount +=  graph.Xscale();
     });
+    context.stroke();
+};
+
+var doLegend = function(graph, rdata) {
+    context.strokeStyle = "#8a9194";
+    var elem = graph.min();
+    while (elem <= graph.max()) { 
+        var Selem = elem - 10; // fudge. See fudge elsewhere.
+        context.moveTo(25, 500  - (Selem * graph.Yscale())); 
+        context.lineTo(1000, 500 - (Selem * graph.Yscale() )); 
+        context.fillText(elem, 10, 500 - (Selem * graph.Yscale()));
+        elem +=1; 
+    } 
+    
+    context.stroke();
+    // now do X(time):
+    var minre = /\d\d:00:00\./;
+    var amount = graph.Xscale();
+    var yOffset = 350;
+    rdata.forEach( function (element, index, array) {
+
+        if(minre.test(element.time)) {
+            var ltime = element.time.match(minre).toString();
+            ltime = ltime.replace(/:00\./, " ");
+            context.moveTo(100+ amount,400);
+            context.lineTo(100 + amount,0);
+//            context.moveTo(100+ amount,yOffset);
+            context.fillText(ltime, 100+amount, yOffset,  graph.Xscale()*4);
+        }
+        amount +=  graph.Xscale();
+    });
+
     context.stroke();
 };
