a771d55660896e96f2ef2366e18a8f778414aa55
Jim Traugott <jtraug@gmail.com>
2016-02-03 10:16:18 -0800

    Added baseline

--- client/drawgraph.js
+++ client/drawgraph.js
@@ -1,37 +1,37 @@
 
 var drawGraph = function() {
     var lineWidth   = 1;
-
+    var baseline = 400;
     context.beginPath();
         context.lineWidth = lineWidth;
     var config = {height: 500, width: 800};
     var graph = new Graph(config);
     graph.setMinMax(rdata);
     //do legend:
-    doLegend(graph, rdata);
+    doLegend(graph, rdata, baseline);
 
     context.beginPath();
-    var fudge = 10;
     context.strokeStyle = '#1D8DB3';
     var amount = graph.Xscale();
-    var yinit = 500 - Math.ceil( graph.Yscale() * (rdata[0].height- fudge));
+    var yinit = baseline - Math.ceil( graph.Yscale() * (rdata[0].height- graph.min()));
     context.moveTo(100+ amount,yinit);
 //    amount +=9;
     rdata.forEach( function (element, index, array) {
-        context.lineTo(100 + amount, 500 -( Math.ceil(graph.Yscale() * (element.height- fudge))));
+        context.lineTo(100 + amount, baseline -( Math.ceil(graph.Yscale() * (element.height- graph.min()))));
         amount +=  graph.Xscale();
     });
     context.stroke();
 };
 
-var doLegend = function(graph, rdata) {
+var doLegend = function(graph, rdata, baseline) {
     context.strokeStyle = "#8a9194";
+    // do Y (feet):
     var elem = graph.min();
     while (elem <= graph.max()) { 
-        var Selem = elem - 10; // fudge. See fudge elsewhere.
-        context.moveTo(25, 500  - (Selem * graph.Yscale())); 
-        context.lineTo(1000, 500 - (Selem * graph.Yscale() )); 
-        context.fillText(elem, 10, 500 - (Selem * graph.Yscale()));
+        var Selem = elem - graph.min();
+        context.moveTo(25, baseline  - (Selem * graph.Yscale())); 
+        context.lineTo(1000, baseline - (Selem * graph.Yscale() )); 
+        context.fillText(elem, 10, baseline - (Selem * graph.Yscale()));
         elem +=1; 
     } 
     
@@ -45,10 +45,10 @@ var doLegend = function(graph, rdata) {
         if(minre.test(element.time)) {
             var ltime = element.time.match(minre).toString();
             ltime = ltime.replace(/:00\./, " ");
-            context.moveTo(100+ amount,400);
+            context.moveTo(100+ amount,baseline);
             context.lineTo(100 + amount,0);
 //            context.moveTo(100+ amount,yOffset);
-            context.fillText(ltime, 100+amount, yOffset,  graph.Xscale()*4);
+            context.fillText(ltime, 100+amount, baseline,  graph.Xscale()*4);
         }
         amount +=  graph.Xscale();
     });
